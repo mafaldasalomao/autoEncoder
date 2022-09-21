1c36e514adab0d1fa3edcb6487b752458707f839
salomao-mafalda <salomaomachadon@gmail.com>
2022-09-21 08:41:41 -0500

    Create Test

--- test.m
+++ test.m
@@ -1,2 +1,20 @@
 %Load the test data.
+load('autoenc.mat')
 XTest = digitTestCellArrayData;
+
+%reconstruct using autoencoder
+
+xReconstructed = predict(autoenc,XTest);
+
+%Show original
+figure;
+for i = 1:20
+    subplot(4,5,i);
+    imshow(XTest{i});
+end
+%show reconstructed
+figure;
+for i = 1:20
+    subplot(4,5,i);
+    imshow(xReconstructed{i});
+end
